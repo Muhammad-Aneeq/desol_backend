4a6382c6d64e1bf341ae7963cc63a8763efd2ca3
Aneeq Khatri <aneeqabdulsamad5761@gmail.com>
2024-07-10 19:28:49 +0500

    updating code for env

--- utils/multer.js
+++ utils/multer.js
@@ -4,6 +4,7 @@ const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "uploads/"); // Directory where files will be stored temporarily
   },
+
   filename: function (req, file, cb) {
     cb(null, file.originalname); // Use original file name
   },
