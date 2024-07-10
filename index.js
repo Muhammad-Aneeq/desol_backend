1d3f8322f412cef20a87b6ec3cd0eac0a5193a9c
Aneeq Khatri <aneeqabdulsamad5761@gmail.com>
2024-07-10 18:27:42 +0500

    code upated again

--- index.js
+++ index.js
@@ -27,14 +27,7 @@ mongoose.connection.on("disconnected", () => {
 });
 
 // Middlewares
-app.all("*", function (req, res) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type,Content-Length, Authorization, Accept,X-Requested-With"
-  );
-  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-});
+
 app.use(
   cors({
     allowedHeaders: ["authorization", "Content-Type"],
