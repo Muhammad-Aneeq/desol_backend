da4fdc1c47764e7abaac19f33054eaf0bdfe841b
Aneeq Khatri <aneeqabdulsamad5761@gmail.com>
2024-07-10 18:19:24 +0500

    code updated

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { register, login } from "../controllers/authController.js";
-
+import cors from "cors";
 const router = express.Router();
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register", cors(), register);
+router.post("/login", cors(), login);
 
 export default router;
